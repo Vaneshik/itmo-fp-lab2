b102904273196d5057294e99d55f58b90c3ef26b
Vaneshik <your.email@example.com>
2025-11-25 14:23:00 +0300

    init project

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject oa-dict "1.0"
+  :description "OpenAddress Hashmap реализация словаря на Clojure"
+  :url "https://github.com/Vaneshik/itmo-fp-lab2"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [org.clojure/test.check "1.1.0"]]
+  :target-path "target/%s"
+  :plugins [[dev.weavejester/lein-cljfmt "0.13.1"],
+            [jonase/eastwood "1.4.3"],
+            [com.github.clj-kondo/lein-clj-kondo "0.2.5"]]
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :test {:injections [(require 'oa-dict.pretty-reporter)
+                                 (require 'oa-dict.assertions)]}})
+
