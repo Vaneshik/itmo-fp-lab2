88e8fcfb9d7298e2dadac0ee99f09aae7f7951cf
Vaneshik <your.email@example.com>
2025-12-02 19:51:07 +0300

    fix

--- src/core.clj
+++ src/core.clj
@@ -1,87 +1,211 @@
-(ns core
-  (:require [protocol :as api]
-            [impl :as impl]))
+(ns core)
+
+;; === Константы ===
+
+(def TABLE_SIZE 32)
+
+;; === Вспомогательные функции ===
+
+(defn- my-reduce-right
+  "Правая свёртка коллекции"
+  [f init coll]
+  (if (empty? coll)
+    init
+    (f (my-reduce-right f init (rest coll)) (first coll))))
+
+(defn- locate-empty-cell
+  "Найти свободную ячейку для вставки с linear probing"
+  [table key start-idx]
+  (loop [idx start-idx
+         attempts 0]
+    (if (>= attempts (count table))
+      nil
+      (let [slot (nth table idx)]
+        (cond
+          (or (nil? slot) (= slot :deleted)) idx
+          (and (vector? slot) (= (first slot) key)) idx
+          :else (recur (mod (inc idx) (count table)) (inc attempts)))))))
+
+(defn- search-key
+  "Найти ячейку с заданным ключом"
+  [table key start-idx]
+  (loop [idx start-idx
+         attempts 0]
+    (if (>= attempts (count table))
+      nil
+      (let [slot (nth table idx)]
+        (cond
+          (nil? slot) nil
+          (and (vector? slot) (= (first slot) key)) idx
+          :else (recur (mod (inc idx) (count table)) (inc attempts)))))))
+
+;; === Протокол ===
+
+(defprotocol Dict
+  "Словарь на основе хеш-таблицы с открытой адресацией"
+  (insert [dict key value])
+  (insert-all [dict pairs])
+  (get-value [dict key])
+  (contains-key? [dict key])
+  (get-keys [dict])
+  (get-values [dict])
+  (get-pairs [dict])
+  (delete [dict key])
+  (filter-dict [dict pred])
+  (map-dict [dict f])
+  (reduce-left [dict f init])
+  (reduce-right [dict f init])
+  (equals-dict? [dict1 dict2])
+  (merge-dict [dict1 dict2])
+  (bind-dict [dict f]))
+
+;; === Реализация ===
+
+(deftype OADict [table]
+  Dict
+
+  (insert [_ key value]
+    (let [idx (mod (hash key) (count table))
+          slot-idx (locate-empty-cell table key idx)]
+      (if slot-idx
+        (OADict. (assoc table slot-idx [key value]))
+        (throw (ex-info "HashMap is full" {:key key :value value})))))
+
+  (insert-all [dict pairs]
+    (reduce (fn ins [d [k v]] (insert d k v)) dict pairs))
+
+  (get-value [_ key]
+    (let [idx (mod (hash key) (count table))
+          slot-idx (search-key table key idx)]
+      (when slot-idx
+        (let [slot (nth table slot-idx)]
+          (when (vector? slot)
+            (second slot))))))
+
+  (contains-key? [_ key]
+    (let [idx (mod (hash key) (count table))
+          slot-idx (search-key table key idx)]
+      (boolean slot-idx)))
+
+  (get-keys [dict]
+    (map first (get-pairs dict)))
+
+  (get-values [dict]
+    (map second (get-pairs dict)))
+
+  (get-pairs [_]
+    (filter vector? table))
+
+  (delete [_ key]
+    (let [idx (mod (hash key) (count table))
+          slot-idx (search-key table key idx)]
+      (if slot-idx
+        (OADict. (assoc table slot-idx :deleted))
+        (OADict. table))))
+
+  (filter-dict [this pred]
+    (->> (get-pairs this)
+         (filter pred)
+         (insert-all (OADict. (vec (repeat TABLE_SIZE nil))))))
+
+  (map-dict [this f]
+    (->> (get-pairs this)
+         (map f)
+         (insert-all (OADict. (vec (repeat TABLE_SIZE nil))))))
+
+  (reduce-left [this f init]
+    (->> (get-pairs this)
+         (reduce f init)))
+
+  (reduce-right [this f init]
+    (->> (get-pairs this)
+         (my-reduce-right f init)))
+
+  (equals-dict? [dict1 dict2]
+    (let [p1 (get-pairs dict1)
+          p2 (get-pairs dict2)]
+      (and
+       (reduce (fn f [acc [k v]] (and acc (= (get-value dict1 k) v))) true p2)
+       (reduce (fn f [acc [k v]] (and acc (= (get-value dict2 k) v))) true p1))))
+
+  (merge-dict [dict1 dict2]
+    (->> (get-pairs dict2)
+         (insert-all dict1)))
+
+  (bind-dict [this f]
+    (reduce-left this
+                 (fn [acc-dict [k v]]
+                   (merge-dict acc-dict (f k v)))
+                 (OADict. (vec (repeat TABLE_SIZE nil)))))
+
+  ;; Стандартные интерфейсы Clojure
+  clojure.lang.ILookup
+  (valAt [dict k]
+    (get-value dict k))
+  (valAt [dict k not-found]
+    (let [v (get-value dict k)]
+      (if (nil? v) not-found v)))
+
+  clojure.lang.Associative
+  (assoc [dict k v]
+    (insert dict k v))
+  (containsKey [dict k]
+    (contains-key? dict k))
+  (entryAt [dict k]
+    (when-let [v (get-value dict k)]
+      (clojure.lang.MapEntry. k v)))
+
+  clojure.lang.IPersistentMap
+  (without [dict k]
+    (delete dict k))
+
+  clojure.lang.Seqable
+  (seq [dict]
+    (seq (map (fn [[k v]] (clojure.lang.MapEntry. k v))
+              (get-pairs dict))))
+
+  clojure.lang.Counted
+  (count [_]
+    (count (filter vector? table)))
+
+  clojure.lang.IPersistentCollection
+  (cons [this o]
+    (cond
+      (instance? clojure.lang.IMapEntry o)
+      (insert this (key o) (val o))
+
+      (and (vector? o) (= 2 (count o)))
+      (insert this (o 0) (o 1))
+
+      (map? o)
+      (insert-all this o)
+
+      (sequential? o)
+      (insert-all this o)
+
+      :else
+      (throw (ex-info "Unsupported element for conj/cons on OADict" {:value o}))))
+
+  (empty [_]
+    (OADict. (vec (repeat TABLE_SIZE nil))))
+
+  (equiv [this other]
+    (cond
+      (instance? OADict other)
+      (equals-dict? this other)
+
+      (map? other)
+      (= (into {} (seq this)) other)
+
+      :else false)))
+
+;; === Публичный API ===
 
 (def empty-dict
   "Пустой словарь с открытой адресацией"
-  (impl/create-empty-dict))
-
-(defn insert
-  "Добавляет пару ключ-значение в словарь"
-  [dict key value]
-  (api/insert dict key value))
-
-(defn insert-all
-  "Добавляет несколько пар в словарь"
-  [dict pairs]
-  (api/insert-all dict pairs))
-
-(defn get-value
-  "Получает значение по ключу"
-  [dict key]
-  (api/get-value dict key))
-
-(defn contains-key?
-  "Проверяет наличие ключа в словаре"
-  [dict key]
-  (api/contains-key? dict key))
-
-(defn get-keys
-  "Возвращает все ключи словаря"
-  [dict]
-  (api/get-keys dict))
-
-(defn get-values
-  "Возвращает все значения словаря"
-  [dict]
-  (api/get-values dict))
-
-(defn get-pairs
-  "Возвращает все пары ключ-значение"
-  [dict]
-  (api/get-pairs dict))
-
-(defn delete
-  "Удаляет элемент по ключу"
-  [dict key]
-  (api/delete dict key))
-
-(defn filter-dict
-  "Фильтрует словарь по предикату"
-  [dict pred]
-  (api/filter-dict dict pred))
-
-(defn map-dict
-  "Преобразует элементы словаря"
-  [dict f]
-  (api/map-dict dict f))
-
-(defn reduce-left
-  "Левая свёртка словаря"
-  [dict f init]
-  (api/reduce-left dict f init))
-
-(defn reduce-right
-  "Правая свёртка словаря"
-  [dict f init]
-  (api/reduce-right dict f init))
-
-(defn equals-dict?
-  "Проверяет равенство двух словарей"
-  [dict1 dict2]
-  (api/equals-dict? dict1 dict2))
-
-(defn merge-dict
-  "Объединяет два словаря (операция моноида)"
-  [dict1 dict2]
-  (api/merge-dict dict1 dict2))
-
-(defn bind-dict
-  "Монадическая операция bind"
-  [dict f]
-  (api/bind-dict dict f))
+  (OADict. (vec (repeat TABLE_SIZE nil))))
 
 (defn dict-from
   "Создает словарь из последовательности пар [key value]"
   [pairs]
-  (api/insert-all empty-dict pairs))
+  (insert-all empty-dict pairs))
